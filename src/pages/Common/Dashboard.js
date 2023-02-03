40fc1b5b4e313d4e7977d95158f8d6bcdd1d8485
kanikaacem <88278472+kanikaacem@users.noreply.github.com>
2023-02-03 17:59:51 +0530

    updating URL and mobile responsive

--- src/pages/Common/Dashboard.js
+++ src/pages/Common/Dashboard.js
@@ -130,7 +130,8 @@ const Dashboard = () => {
                                                 Premium Posting : {user && user.employer_plan} credits
                                             </Typography> */}
 
-                                            <Stack direction="row" gap={2} sx={{ cursor: "pointer" }} onClick={() => window.location.href = window.location.origin + '/employer-dashboard/account-setting'}>
+                                            <Stack direction="row" gap={2} sx={{ cursor: "pointer" }}
+                                                onClick={() => window.location.href = window.location.origin + '/employer-dashboard/account-setting'}>
                                                 <PersonIcon />
                                                 <Typography component="div" sx={{ fontSize: "14px" }}>
                                                     Account Setting
