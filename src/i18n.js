6d58b346bc9b4c5c5f542a1736ac12ddb74234c3
kanikaacem <kanika.np@acem.edu.in>
2023-05-08 18:00:03 +0530

    hinglish language translation

--- src/i18n.js
+++ src/i18n.js
@@ -6,8 +6,7 @@ import { initReactI18next } from "react-i18next";
 
 import translationEN from "./locales/en/translation.json";
 import translationHE from "./locales/hn/translation.json";
-
-
+import translationEHN from "./locales/ehn/translation.json";
 //Creating object with the variables of imported translation files
 const resources = {
     en: {
@@ -16,10 +15,12 @@ const resources = {
     hn: {
         translation: translationHE,
     },
+    ehn: {
+        translation: translationEHN
+    }
 };
 
 //i18N Initialization
-
 i18n
     .use(initReactI18next)
     .init({
