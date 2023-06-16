94f6ee65b7414d98f58a2de8a017335722452160
swetanks <swetanks@beelabs.technology>
2023-06-16 07:05:09 +0530

    add pixel ID

--- gtag-replace-dev.sh
+++ gtag-replace-dev.sh
@@ -5,6 +5,7 @@ file_path="build/index.html"
 
 # Set the Google tag ID from the Google script
 google_tag_id="G-EJVXEP24LG"
+fb_pixel_id="765167175359816"
 
 # Check if the file exists
 if [ ! -f "$file_path" ]; then
@@ -14,5 +15,7 @@ fi
 
 # Replace the Google tag ID in the index.html file
 sed -i "s/G-H8BGSJ85MW/$google_tag_id/g" "$file_path"
+# Replace the FB Pixel ID in the index.html file
+sed -i "s/810688943559923/$fb_pixel_id/g" "$file_path"
 
 echo "Google tag ID replaced successfully!"
