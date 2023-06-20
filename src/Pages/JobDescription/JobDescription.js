64ba14e4b811a1b420a3e404f3b805c53a3928e3
swetanks <swetanks@beelabs.technology>
2023-06-20 16:07:20 +0530

    share job comms chnage

--- src/Pages/JobDescription/JobDescription.js
+++ src/Pages/JobDescription/JobDescription.js
@@ -11,10 +11,7 @@ import { useState, useEffect } from "react";
 import moment from 'moment';
 import ShareJob from "../Common/ShareJob";
 import { useTranslation } from "react-i18next";
-<<<<<<< HEAD
 import { Helmet } from "react-helmet";
-=======
->>>>>>> 52aa376c13cd35644ff4d033cae5ba12dfe54cf4
 import ApplyForJobWebButton from "../Common/ApplyForJobWebButton";
 const JobDescription = () => {
     const location = useLocation();
