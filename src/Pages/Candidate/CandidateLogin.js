f808718f4badfd968ae87b42b8a93a387fb3486c
kanikaacem <dev_frontend@beelabs.technology>
2023-06-29 15:40:12 +0530

    Employer Login Desktop

--- src/Pages/Candidate/CandidateLogin.js
+++ src/Pages/Candidate/CandidateLogin.js
@@ -1,5 +1,5 @@
 import { postRequest } from "../../utils/ApiRequests";
-import { Box, Button, Stack, Container, Typography, TextField } from "@mui/material";
+import { Box, Button, Stack, Typography, TextField, Container } from "@mui/material";
 import PageTopSection from "../Common/PageTopSection";
 import FormLabel from "../Common/FormLabel";
 import LoaderScreen from "../Common/LoaderScreen";
@@ -127,6 +127,77 @@ const CandidateLogin = () => {
                 </Box>
 
             </Container >
+            {/* <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    kjfkljdl
+                </Grid>
+                <Grid item xs={6}>
+                    <Box className="CandidateLoginSection" sx={{
+                        padding: "24px 20px"
+
+                    }}>
+                        <Formik
+                            initialValues={defaultValue}
+                            validationSchema={CandidateMobileNumberValidation}
+                            onSubmit={handleSubmit}
+                        >
+                            {({ errors, touched }) => (
+                                <Form className="CandidateLoginForm">
+                                    <Stack direction="column" gap={2}>
+                                        <FormLabel LableFor="email_address" LableText={t('WHATSAPP_MOBILE_NUMBER')} />
+                                        <Field
+                                            sx={{
+                                                "& fieldset": { border: 'none' },
+                                            }}
+                                            error={errors.mobile_number && touched.mobile_number}
+                                            as={TextField}
+                                            id="mobile_number"
+                                            type="text"
+                                            name="mobile_number"
+                                            fullWidth
+                                            InputProps={{
+                                                placeholder: "Enter Mobile Number",
+                                                style: { color: "#999999", fontWeight: 400 },
+                                            }}
+                                        />
+                                        {errors.mobile_number && touched.mobile_number && <Error text={errors.mobile_number} />}
+                                    </Stack>
+                                    <Button variant="contained"
+                                        type="submit"
+                                        sx={{
+                                            background: "#FF671F",
+                                            borderRadius: "33px",
+                                            textTransform: "capitalize",
+                                            margin: "30px 0px",
+                                            width: "100%",
+                                            height: "43px",
+                                            fontSize: { "xs": "0.8rem", "sm": "0.8rem", "md": "1.5rem", "lg": "1.5rem", "xl": "1.5rem" },
+                                            "&:hover": {
+                                                background: "#FF671F",
+                                            }
+                                        }
+                                        }> {t('SEND_OTP')}</Button >
+
+                                    <Typography
+                                        sx={{
+                                            marginTop: "40px",
+                                            fontSize: { xs: "1rem", sm: "1rem", md: "1rem", lg: "1rem", xl: "1rem" },
+                                            fontWeight: "500",
+                                            textAlign: "center"
+                                        }}
+                                    >
+                                        By continuing, you agree to the <a href={`${window.location.origin}/${currentLanguage}/terms-and-conditions`} style={{ color: "#0D99FF", textDecoration: "none" }}>Terms & Conditions</a> and
+                                        <a href={`${window.location.origin}/${currentLanguage}/privacy-policy`} style={{ color: "#0D99FF", textDecoration: "none" }}> Privacy & Policy</a> of JobsYahan.
+                                    </Typography>
+
+                                </Form>
+                            )}
+                        </Formik>
+                    </Box>
+                </Grid>
+
+            </Grid> */}
+
             <Footer />
         </Box >
 
--- src/Pages/Employer/EmployerLogin.js
+++ src/Pages/Employer/EmployerLogin.js
@@ -1,5 +1,5 @@
 import { postRequest } from "../../utils/ApiRequests";
-import { Box, Typography, Button, Container, useMediaQuery } from "@mui/material";
+import { Box, Typography, Button, Container, useMediaQuery, Grid } from "@mui/material";
 import { employerLoginValidationSchema } from "../../Validation/EmployerValidation";
 import Error from "../../ThemeComponent/Common/Error";
 import FormLabel from "../Common/FormLabel";
@@ -51,90 +51,182 @@ const EmployerLogin = () => {
                 <meta property="og:image" content="https://jobyahanp.s3.ap-south-1.amazonaws.com/images/logo/logo_og.png" />
                 <meta property="og:url" content={`${window.location.origin}/${currentLanguage}/employer-login`} />
             </Helmet>
-            <Box sx={{ minHeight: "100vh" }}>
+            <Box>
                 <Header showButton={false} />
-
-                <Container maxWidth={isDesktop ? "md" : false} sx={{ padding: "0px" }}>
-                    <Box
-                        className="EmployerLoginFormTopSection"
-                        sx={{
-                            // height: "430px",
-                            padding: "20px 20px 0px",
-                            backgroundImage: "linear-gradient(#ffffff 30%,#f3bb7a )",
-                            position: "relative"
-                        }}
-                    >
-                        {/* <ArrowBackIcon sx={{ cursor: "pointer" }} onClick={() => { window.history.back(); }} /> */}
-                        <Typography
-                            variant="h1"
-                            component="h2"
-                            sx={{
-                                fontSize: "1.3rem",
-                                marginTop: "50px",
-                                textAlign: "center",
-                                fontWeight: "600"
-                            }}
-                        >
-                            {t('EMPR_LOGIN_BANNER')}
-                        </Typography>
-                        <Box sx={{
-                            width: {
-                                "xs": "100%", "sm": "100%",
-                                "md": "450px", "xl": "450px", "lg": "450px"
-                            },
-                            margin: "50px auto 0px"
+                {isDesktop &&
+                    <Grid container sx={{
+                        background: `linear-gradient(#ffffff 10% , #f3bb7a )`,
+                        height: "inherit",
+                        boxSizing: "border-box",
+                        padding: "20px"
+                    }} >
+                        <Grid item lg={6} sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center"
                         }}>
-                            <img
-                                src="https://jobyahanp.s3.ap-south-1.amazonaws.com/images/logo/employer_login.png"
-                                width="100%"
-                                alt="find-candidates"
-                            />
+                            <Box sx={{ width: "560px" }}>
+                                <img
+                                    src="https://jobyahanp.s3.ap-south-1.amazonaws.com/images/logo/employer_login.png"
+                                    width="100%"
+                                    height="100%" />
+                            </Box>
+                        </Grid>
+                        <Grid item lg={6} sx={{
+                            boxSizing: "border-box",
+                            padding: "50px 200px"
+                        }}>
+
+                            <Box className="EmployerLoginFormSection" sx={{
+                                border: "1px solid #D8D8D8",
+                                borderRadius: "15px",
+                                padding: "30px 40px",
+                                background: "#ffffff"
+                            }}>
+                                <Typography
+                                    variant="h1"
+                                    component="h2"
+                                    sx={{
+                                        fontSize: "1.5rem",
+                                        margin: "20px 0px",
+                                        fontWeight: "700",
+                                        lineHeight: "1.5"
+
+                                    }}
+                                >
+                                    {t('EMPR_LOGIN_BANNER')}
+                                </Typography>
+
+                                <Formik
+                                    initialValues={defaultValue}
+                                    validationSchema={employerLoginValidationSchema}
+                                    onSubmit={handleSubmit}
+                                >
+                                    {({ errors, touched }) => (
+                                        <Form className="EmployerLoginForm" >
+                                            <Box className="FormGroup">
+                                                <FormLabel LableText={t('EMPR_USERNAME')} LableFor="user_email" sx={{ fontSize: "14px !important" }} />
+                                                <Field id="user_email"
+                                                    type="text" placeholder={t('EMPR_USERNAME')}
+                                                    name="user_email"
+                                                    className="custom-text-field" />
+                                                {errors.user_email && touched.user_email && <Error text={errors.user_email} />}
+
+                                            </Box>
+                                            <Box className="FormGroup">
+                                                <FormLabel LableText={t('EMPR_PASSWORD')} LableFor="password" sx={{ fontSize: "14px !important" }} />
+                                                <Field id="password"
+                                                    type="password" placeholder={t('EMPR_PASSWORD')}
+                                                    name="password"
+                                                    className="custom-text-field" />
+                                                {errors.password && touched.password && <Error text={errors.password} />}
+
+                                            </Box>
+
+                                            <Button variant="contained" type="submit" className="OrangeButton" sx={{
+                                                margin: "20px 0px !important"
+                                            }} >{t('SUBMIT')}</Button>
+
+                                        </Form>
+
+                                    )}
+
+                                </Formik>
+                            </Box>
+                        </Grid>
+                        <Box sx={{ textAlign: 'center', width: '100%' }}>
+                            <a href="/find-candidates" className="back-button-link">
+                                <p>{t('BACK')}</p>
+                            </a>
                         </Box>
-                    </Box>
-                    <Box className="EmployerLoginFormSection" sx={{ padding: "30px 20px" }}>
-                        <Formik
-                            initialValues={defaultValue}
-                            validationSchema={employerLoginValidationSchema}
-                            onSubmit={handleSubmit}
-                        >
-                            {({ errors, touched }) => (
-                                <Form className="EmployerLoginForm" >
-                                    <Box className="FormGroup">
-                                        <FormLabel LableText={t('EMPR_USERNAME')} LableFor="user_email" />
-                                        <Field id="user_email"
-                                            type="text" placeholder={t('EMPR_USERNAME')}
-                                            name="user_email"
-                                            className="custom-text-field" />
-                                        {errors.user_email && touched.user_email && <Error text={errors.user_email} />}
 
-                                    </Box>
-                                    <Box className="FormGroup">
-                                        <FormLabel LableText={t('EMPR_PASSWORD')} LableFor="password" />
-                                        <Field id="password"
-                                            type="password" placeholder={t('EMPR_PASSWORD')}
-                                            name="password"
-                                            className="custom-text-field" />
-                                        {errors.password && touched.password && <Error text={errors.password} />}
+                    </Grid>}
 
-                                    </Box>
+                {
+                    !isDesktop && (
+                        <Container maxWidth={isDesktop ? "md" : false} sx={{ padding: "0px" }}>
+                            <Box
+                                className="EmployerLoginFormTopSection"
+                                sx={{
+                                    // height: "430px",
+                                    padding: "20px 20px 0px",
+                                    backgroundImage: "linear-gradient(#ffffff 30%,#f3bb7a )",
+                                    position: "relative"
+                                }}
+                            >
+                                {/* <ArrowBackIcon sx={{ cursor: "pointer" }} onClick={() => { window.history.back(); }} /> */}
+                                <Typography
+                                    variant="h1"
+                                    component="h2"
+                                    sx={{
+                                        fontSize: "1.3rem",
+                                        marginTop: "50px",
+                                        textAlign: "center",
+                                        fontWeight: "600"
+                                    }}
+                                >
+                                    {t('EMPR_LOGIN_BANNER')}
+                                </Typography>
+                                <Box sx={{
+                                    width: {
+                                        "xs": "100%", "sm": "100%",
+                                        "md": "450px", "xl": "450px", "lg": "450px"
+                                    },
+                                    margin: "50px auto 0px"
+                                }}>
+                                    <img
+                                        src="https://jobyahanp.s3.ap-south-1.amazonaws.com/images/logo/employer_login.png"
+                                        width="100%"
+                                        alt="find-candidates"
+                                    />
+                                </Box>
+                            </Box>
+                            <Box className="EmployerLoginFormSection" sx={{ padding: "30px 20px" }}>
+                                <Formik
+                                    initialValues={defaultValue}
+                                    validationSchema={employerLoginValidationSchema}
+                                    onSubmit={handleSubmit}
+                                >
+                                    {({ errors, touched }) => (
+                                        <Form className="EmployerLoginForm" >
+                                            <Box className="FormGroup">
+                                                <FormLabel LableText={t('EMPR_USERNAME')} LableFor="user_email" />
+                                                <Field id="user_email"
+                                                    type="text" placeholder={t('EMPR_USERNAME')}
+                                                    name="user_email"
+                                                    className="custom-text-field" />
+                                                {errors.user_email && touched.user_email && <Error text={errors.user_email} />}
 
-                                    <Button variant="contained" type="submit" className="OrangeButton" sx={{
-                                        margin: "20px 0px !important"
-                                    }} >{t('SUBMIT')}</Button>
-                                    <a href="/find-candidates" className="back-button-link">
-                                        <p>{t('BACK')}</p>
-                                    </a>
-                                </Form>
+                                            </Box>
+                                            <Box className="FormGroup">
+                                                <FormLabel LableText={t('EMPR_PASSWORD')} LableFor="password" />
+                                                <Field id="password"
+                                                    type="password" placeholder={t('EMPR_PASSWORD')}
+                                                    name="password"
+                                                    className="custom-text-field" />
+                                                {errors.password && touched.password && <Error text={errors.password} />}
 
-                            )}
+                                            </Box>
 
-                        </Formik>
-                    </Box>
+                                            <Button variant="contained" type="submit" className="OrangeButton" sx={{
+                                                margin: "20px 0px !important"
+                                            }} >{t('SUBMIT')}</Button>
+                                            <a href="/find-candidates" className="back-button-link">
+                                                <p>{t('BACK')}</p>
+                                            </a>
+                                        </Form>
 
-                </Container>
+                                    )}
+
+                                </Formik>
+                            </Box>
+
+                        </Container>
+                    )
+                }
             </Box>
             <Footer />
-        </Box>
+        </Box >
     </>);
 };
 
